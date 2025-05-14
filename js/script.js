5603e9623eb53186d55ff0be15b72150ac99086a
Phim24h <phamhuytuyenjp@gmail.com>
2025-05-14 20:05:14 +0700

    lần sửa đổi tiếp theo

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function openNav() {
+  document.getElementById("sidenav").classList.add("active");
+  document.getElementById("overlay").classList.add("active");
+}
+
+function closeNav() {
+  document.getElementById("sidenav").classList.remove("active");
+  document.getElementById("overlay").classList.remove("active");
+}
